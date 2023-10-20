afb23752c664c2dee249ca92f61dec8de488c5a7
Cyandied <Thea-Johansen@outlook.dk>
2023-10-20 18:59:30 +0200

    Push to transfer

--- tilegame/tilegame.js
+++ tilegame/tilegame.js
@@ -14,41 +14,44 @@ const naked_data = {
     "left": 0
 }
 
-const colors = ["one","two","three","four","five","six"]
+const classes = ["one", "two", "three", "four", "five", "six"]
+
+const gameboard_size = 51
 
 let round = 0
 let points = 0
 let tiles_placed = 0
+let discard_combo = 0
 
-color_select.addEventListener("change",e=>{
+color_select.addEventListener("change", e => {
     const tile_style = color_select.value
-    tile_style_sheet.setAttribute("href",`tile_styles/${tile_style}.css`)
+    tile_style_sheet.setAttribute("href", `tile_styles/${tile_style}.css`)
 })
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
-function show_popup(title){
+function show_popup(title) {
     const title_part = document.createElement("h2")
     title_part.innerHTML = title
     popup.appendChild(title_part)
     popup.classList.add("display")
-    sleep(3000).then(()=>{
+    sleep(3000).then(() => {
         popup.classList.remove("display")
-        sleep(500).then(()=>{
+        sleep(500).then(() => {
             popup.removeChild(title_part)
         })
     })
 }
 
-game_buttons.forEach(button =>{
-    button.addEventListener("click", e=>{
+game_buttons.forEach(button => {
+    button.addEventListener("click", e => {
         const action = e.target.dataset.action
-        if(action == "rotate"){
+        if (action == "rotate") {
             action_rotate_tile()
         }
-        else if(action == "discard"){
+        else if (action == "discard") {
             action_discard_tile()
         }
     })
@@ -64,36 +67,39 @@ function rand_tile_type() {
     return type
 }
 
-function rotate_tile(old_data){
-    const new_data = {"top":old_data.left,"bottom":old_data.right,"right":old_data.top, "left":old_data.bottom}
+function rotate_tile(old_data) {
+    const new_data = {
+        "top": old_data.left,
+        "bottom": old_data.right,
+        "right": old_data.top,
+        "left": old_data.bottom,
+    }
     return make_tile(new_data)
 }
 
-let discard_combo = 0
-
-function action_discard_tile(){
+function action_discard_tile() {
     discard_combo++;
-    if(discard_combo > 3){
+    if (discard_combo > 3) {
         game_over_screen.classList.remove("hidden")
         return
     }
     const discard_counter = document.querySelector("#counter")
     discard_counter.innerHTML = discard_combo
-    tile_to_place.replaceChild(make_tile(rand_tile_type()),tile_to_place.children[0])
+    tile_to_place.replaceChild(make_tile(rand_tile_type()), tile_to_place.children[0])
 }
 
-function action_rotate_tile(){
+function action_rotate_tile() {
     const tile = tile_to_place.children[0]
     const type = JSON.parse(tile.dataset.type)
     const rotated_tile = rotate_tile(type)
-    tile_to_place.replaceChild(rotated_tile,tile_to_place.children[0])
+    tile_to_place.replaceChild(rotated_tile, tile_to_place.children[0])
 }
 
-document.addEventListener("keypress",e=>{
-    if(e.key == "r"){
+document.addEventListener("keypress", e => {
+    if (e.key == "r") {
         action_rotate_tile()
     }
-    else if(e.key == "d"){
+    else if (e.key == "d") {
         action_discard_tile()
     }
 })
@@ -103,49 +109,49 @@ function get_adjacant_tile_info(col, row) {
     let bot_tile = 0
     let r_tile = 0
     let l_tile = 0
-    if(row-1 >= 0){
-        top_tile = JSON.parse(gameboard.children[row-1].children[col].dataset.type).bottom
+    if (row - 1 >= 0) {
+        top_tile = JSON.parse(gameboard.children[row - 1].children[col].dataset.type).bottom
     }
-    if(row+1 < matrix.length){
-        bot_tile = JSON.parse(gameboard.children[row+1].children[col].dataset.type).top
+    if (row + 1 < gameboard_size) {
+        bot_tile = JSON.parse(gameboard.children[row + 1].children[col].dataset.type).top
     }
-    if(col+1 < matrix.length){
-        r_tile = JSON.parse(gameboard.children[row].children[col+1].dataset.type).left
+    if (col + 1 < gameboard_size) {
+        r_tile = JSON.parse(gameboard.children[row].children[col + 1].dataset.type).left
     }
-    if(col-1 >= 0){
-        l_tile = JSON.parse(gameboard.children[row].children[col-1].dataset.type).right
+    if (col - 1 >= 0) {
+        l_tile = JSON.parse(gameboard.children[row].children[col - 1].dataset.type).right
     }
 
     return { "top": top_tile, "bottom": bot_tile, "right": r_tile, "left": l_tile }
 }
 
-function place_tile(target_pos,tile){
+function place_tile(target_pos, tile) {
     tiles_placed++;
-    gameboard.children[target_pos.row].replaceChild(tile,gameboard.children[target_pos.row].children[target_pos.col])
+    gameboard.children[target_pos.row].replaceChild(tile, gameboard.children[target_pos.row].children[target_pos.col])
     tile_to_place.appendChild(make_tile(rand_tile_type()))
 }
 
-function check_poins(tile_type, tile_attrib,adjacent_types){
+function check_poins(tile_type, tile_attrib, adjacent_types) {
     let points = 5
     let touching_tiles = 0
-    for(const type in adjacent_types){
-        if(adjacent_types[type] > 0){
+    for (const type in adjacent_types) {
+        if (adjacent_types[type] > 0) {
             touching_tiles++;
         }
     }
-    if(touching_tiles == 4){
+    if (touching_tiles == 4) {
         touching_tiles += 4
     }
-    else if(touching_tiles > 0){
+    else if (touching_tiles > 0) {
         touching_tiles--
     }
 
-    if(tiles_placed%10 == 0){
+    if (tiles_placed % 10 == 0) {
         points += 20
     }
-    
-    points += touching_tiles*10
-    if(points > 30){
+
+    points += touching_tiles * 10
+    if (points > 30) {
         show_popup(`${points} point!`)
     }
     return points
@@ -154,25 +160,30 @@ function check_poins(tile_type, tile_attrib,adjacent_types){
 function check_if_can_place(target_type, target_pos) {
     if (JSON.stringify(target_type) != JSON.stringify(naked_data)) {
         show_popup("Du må kun placerer brikker i tomme felter!")
-        return false
+        return
     }
     const tile = tile_to_place.children[0]
     const type = JSON.parse(tile.dataset.type)
     const attrib = tile.dataset.attrib
 
     const adjacent_types = get_adjacant_tile_info(target_pos.col, target_pos.row)
-    if((round > 0) == (JSON.stringify(adjacent_types) != JSON.stringify(naked_data))){
-        if ((type.top == adjacent_types.top || adjacent_types.top == 0) && (type.bottom == adjacent_types.bottom || adjacent_types.bottom == 0) && (type.right == adjacent_types.right || adjacent_types.right == 0) && (type.left == adjacent_types.left || adjacent_types.left == 0)) {
+    if ((round > 0) == (JSON.stringify(adjacent_types) != JSON.stringify(naked_data))) {
+        if (
+            (type.top == adjacent_types.top || adjacent_types.top == 0) &&
+            (type.bottom == adjacent_types.bottom || adjacent_types.bottom == 0) &&
+            (type.right == adjacent_types.right || adjacent_types.right == 0) &&
+            (type.left == adjacent_types.left || adjacent_types.left == 0)
+        ) {
             round++;
-            place_tile(target_pos,tile)
-            points += check_poins(type,attrib,adjacent_types)
+            place_tile(target_pos, tile)
+            points += check_poins(type, attrib, adjacent_types)
             points_counter.innerHTML = points
         }
-        else{
+        else {
             show_popup("En eller flere sider macher ikke!")
         }
     }
-    else{
+    else {
         show_popup("Brik skal placeres ved siden af andre brikker!")
     }
 
@@ -186,11 +197,11 @@ function make_tile(data, col, row) {
     tile.dataset.type = JSON.stringify(data)
     tile.dataset.pos = JSON.stringify({ "col": col, "row": row })
     tile.dataset.attrib = "none"
-    if(JSON.stringify(data) != JSON.stringify(naked_data)){
-        tile.classList.add(`top-${colors[data.top-1]}`)
-        tile.classList.add(`bottom-${colors[data.bottom-1]}`)
-        tile.classList.add(`right-${colors[data.right-1]}`)
-        tile.classList.add(`left-${colors[data.left-1]}`)
+    if (JSON.stringify(data) != JSON.stringify(naked_data)) {
+        tile.classList.add(`top-${classes[data.top - 1]}`)
+        tile.classList.add(`bottom-${classes[data.bottom - 1]}`)
+        tile.classList.add(`right-${classes[data.right - 1]}`)
+        tile.classList.add(`left-${classes[data.left - 1]}`)
     }
     tile.addEventListener("click", e => {
         const clicked_tile = e.target
@@ -208,22 +219,9 @@ tile_to_place.appendChild(make_tile({
     "left": 4
 }, null, null))
 
-function make_div() {
-    return document.createElement("div")
-}
-let row = []
-const matrix = []
-
-for (let i = 1; i <= 51 * 51; i++) {
-    row.push(0)
-    if (i % 51 == 0 && i != 0) {
-        matrix.push(row)
-        row = []
-    }
-}
-for (let row = 0; row < matrix.length; row++) {
-    const div = make_div()
-    for (let col = 0; col < matrix[row].length; col++) {
+for (let row = 0; row < gameboard_size; row++) {
+    const div = document.createElement("div")
+    for (let col = 0; col < gameboard_size; col++) {
         div.appendChild(make_tile(naked_data, col, row))
     }
     gameboard.appendChild(div)
