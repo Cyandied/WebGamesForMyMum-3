52b296e11eb266f1ac5790407ab51709277b7c12
Cyandied <Thea-Johansen@outlook.dk>
2024-11-05 23:12:21 +0100

    FLOWERS!
    There are now visible flowers!
    Also update the way i store levels but I am way more proud of the flowers tbh

--- flowerGame/flowerGame.js
+++ flowerGame/flowerGame.js
@@ -16,12 +16,24 @@ const gardenWrapper = document.querySelector("#gardenWrapper");
 
 const playGrid = [];
 
-for (let row = 0; row < level.gridy; row++) {
+for (let r = 0; r < level.gridy; r++) {
     const row = rowTempalte.cloneNode(true);
     const rowPlay = [];
     row.classList.remove("hidden");
     for (let col = 0; col < level.gridx; col++) {
+        const cellInfo = level.startState[r][col];
         const plot = document.createElement("div");
+        if(cellInfo.includes("x")){
+            plot.classList.add("disabled");
+        }
+        else if(cellInfo.includes("h")){
+            plot.classList.add("highlight");
+        }
+        else if (cellInfo.includes("f")){
+            const flower = level.startFlowers[parseInt(cellInfo.split(",")[1])-1];
+            createFlower(flower,plot);
+        }
+        plot.dataset.state = cellInfo;
         plot.classList.add = "plot";
         rowPlay.push(plot);
         row.appendChild(plot);
@@ -30,8 +42,24 @@ for (let row = 0; row < level.gridy; row++) {
     gardenWrapper.appendChild(row);
 }
 
-function createFlower(flower){
-    
+function createFlower(flower,target){
+    const petal = document.createElement("div");
+    petal.classList.add("flowerdot");
+    const petals = [];
+    const petalAm = flower.petalAmount;
+    for (let i = 0; i < petalAm; i++) {
+        petals.push(petal.cloneNode(true));
+        petals[i].classList.add(flower.petals[i].color.join(""));
+        petals[i].style["transform-origin"] = `right ${15}px`;
+        petals[i].style.transform = `rotate(${(i+0.00000001)/petalAm*360}deg)`;
+        petals[i].style.right = `${45}px`;
+        petals[i].style.top = `${45-15}px`;
+        target.appendChild(petals[i]);   
+    }
+}
+
+function removeFlower(target){
+    target.innterHTML="";
 }
 
 
--- flowerGame/levels.js
+++ flowerGame/levels.js
@@ -1,18 +1,13 @@
 export {levels,setComplete,Flower,Level};
 
 class Flower {
-    constructor(petalAmount, colors){
+    constructor(petalAmount,universal="", petal1="",petal2="",petal3="",petal4="",petal5=""){
         this.petalAmount = petalAmount;
-        this.petals = [];
-        if (colors.length == 1){
-            for (let index = 0; index < this.petalAmount; index++) {
-                this.petals.push(new Petal(colors[0]));
-            }
+        if(petal1==""){
+            this.petals = [new Petal(universal.split(",")),new Petal(universal.split(",")),new Petal(universal.split(",")),new Petal(universal.split(",")),new Petal(universal.split(","))];
         }
         else {
-            colors.forEach(color => {
-                this.petals.push(new Petal(color))
-            });
+            this.petals = [new Petal(petal1.split(",")),new Petal(petal2.split(",")),new Petal(petal3.split(",")),new Petal(petal4.split(",")),new Petal(petal5.split(","))];
         }
     }
     combine(otherFlower){
@@ -42,7 +37,7 @@ class Petal {
             "r":0,
             "g":0,
             "b":0,
-            "bl":0,
+            "k":0,
             "w":0
         }
         this.color.forEach(c => {
@@ -57,7 +52,7 @@ class Petal {
                 newColor.push(c);
                 return newColor;
             }
-            else if(c.match("bl") && counter["w"] > 0 && counter["bl"] > 0){
+            else if(c.match("k") && counter["w"] > 0 && counter["k"] > 0){
                 return newColor;
             }
             else if (counter[c] > 0){
@@ -69,34 +64,48 @@ class Petal {
 }
 
 class Level {
-    constructor(gridx,gridy,petals,pflowers,order,htiles,dtiles,msgb,msga,lvlComplete) {
-        this.gridx = gridx,
-        this.gridy = gridy,
-        this.pAmount = petals,
-        this.playerFlowers = [],
-        pflowers.forEach(c=> {
-            this.playerFlowers.push(new Flower(petals,c));
-        })
-        this.order = [],
-        order.forEach(c=> {
-            this.order.push(new Flower(petals,c));
-        })
-        this.highlightTiles = htiles,
-        this.disableTiles = dtiles,
-        this.messageBefore = msgb,
-        this.messageAfter = msga,
-        this.levelComplete = lvlComplete
+    constructor(gridx,gridy,petals,pflowers,order,msgb = NaN,msga = NaN,startState = NaN,startFlowers = NaN) {
+        this.gridx = gridx;
+        this.gridy = gridy;
+        this.pAmount = petals;
+        this.playerFlowers = pflowers;
+        this.order = order;
+        this.messageBefore = msgb;
+        this.messageAfter = msga;
+        this.startState = startState;
+        this.startFlowers = startFlowers;
+        this.levelComplete = false;
     }
 
 }
 
 const levelList = document.querySelector(".levelList");
 
-const testLevel = new Level(3,3,3,[["r"]],[["r","b"]],[[0,1],[1,0],[1,2],[2,1]],[[0,1],[0,2],[2,1],[2,2]],"Place red flower in any highlighted tile to combine their colors","Well done!",false);
+const testLevel = new Level(
+    3,3,3,
+    {
+        "r":1,
+        "g":0,
+        "b":0,
+        "k":0,
+        "w":0
+    },
+    [
+        new Flower(3,"r,b")
+    ],
+    "Please place the red flower on any highlighted tiles",
+    "Well done!",
+    [
+        ["x","h","x"],
+        ["h","f,1","h"],
+        ["x","h","x"]
+    ],
+    [new Flower(3,"b")]
+);
 
 const levels = {
     "Tutorial":
-    [new Level(3,3,3,[["r"]],[["r","b"]],[[0,1],[1,0],[1,2],[2,1]],[[0,1],[0,2],[2,1],[2,2]],"Place red flower in any highlighted tile to combine their colors","Well done!",false),testLevel,testLevel,testLevel,testLevel],
+    [testLevel,testLevel,testLevel,testLevel],
     "Easy":
     [testLevel,testLevel,testLevel,testLevel,testLevel,testLevel,testLevel,testLevel,testLevel,testLevel,testLevel],
     "Medium":
@@ -126,7 +135,7 @@ if(levelList){
        if(item){
            item = JSON.parse(item);
            for (let i = 0; i < item.length; i++) {
-               levels[levelName][i].lvlComplete = item[i];
+               levels[levelName][i].levelComplete = item[i];
            }
        }
        else {
@@ -151,4 +160,5 @@ if(levelList){
        document.body.appendChild(newList);
    })
 }
+
 
