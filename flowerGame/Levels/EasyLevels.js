7d62fcfaf8bcf2bc58b5951638437ec406e260f1
Cyandied <Thea-Johansen@outlook.dk>
2024-11-08 12:39:33 +0100

    Made a level a bit more open

--- flowerGame/Levels/EasyLevels.js
+++ flowerGame/Levels/EasyLevels.js
@@ -5,7 +5,7 @@ const Elevel1 = new Level(
     3,3,3,
     {
         "r":1,
-        "g":0,
+        "g":1,
         "b":0,
         "k":1,
         "w":0
@@ -15,10 +15,10 @@ const Elevel1 = new Level(
     "Well done!",
     [
         ["f,1","",""],
-        ["","","f,2"],
+        ["","",""],
         ["","",""]
     ],
-    [new Flower(3,"w"),new Flower(3,"g")]
+    [new Flower(3,"w")]
 )
 
 const Elevels = [Elevel1];
